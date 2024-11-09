8d7dc681e556a88e3882c91f32376b1e27b74592
eshaan <eshdesh@null.net>
2024-11-09 00:45:15 -0600

    added support for multiple equations

--- graph.py
+++ graph.py
@@ -1,23 +1,31 @@
 import pygame, sys
 
-eq = input("y = ")
+eq = []
+
+while True:
+    eq.append(input("y = "))
+    if (eq[-1] == "graph"):
+        eq.remove("graph")
+        break
 
 pygame.init()
 screen_size = 500
-screen = pygame.display.set_mode((screen_size, screen_size))
+screen = pygame.display.set_mode((500, 500))
+
+def yy(y):
+  return (-y + screen_size)
 
 def px(x,y):
     screen.set_at((int(x), int(y)), (255, 255, 255))
 
-def yy(y):
-    return (-y + screen_size)
 
-def graph():
+def graph(i):
     for x in range(screen_size):
-        y=eval(eq)
+        y = eval(eq[i])
         px(x, yy(y))
 
 while True:
     screen.fill((0, 0, 0))
-    graph()
+    for i in range(len(eq)):
+        graph(i)
     pygame.display.flip()
