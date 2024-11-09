736cde784370060d0a7d9a10b408b0bf2290c561
eshaan <eshdesh@null.net>
2024-11-08 23:50:37 -0600

    everything's working perfectly!!

--- graph.py
+++ graph.py
@@ -7,14 +7,17 @@ screen_size = 500
 screen = pygame.display.set_mode((screen_size, screen_size))
 
 def px(x,y):
-    screen.set_at((x, y), (255, 255, 255))
+    screen.set_at((int(x), int(y)), (255, 255, 255))
+
+def yy(y):
+    return (-y + screen_size)
 
 def graph():
     for x in range(screen_size):
-        y = eval(eq)
-        px(x,y)
+        y=eval(eq)
+        px(x, yy(y))
 
 while True:
     screen.fill((0, 0, 0))
-    triRender(p1, p2, p3)
+    graph()
     pygame.display.flip()
