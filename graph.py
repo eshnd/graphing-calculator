f4aee356c67805001489ee37964fdede16c4483a
eshon <eshaan@Laptop-Probably.local>
2024-11-20 09:35:56 -0600

    a

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
-import pygame, sys
+import pygame
+import sys
 
 eq = []
 
 while True:
     eq.append(input("y = "))
-    if (eq[-1] == "graph"):
+    if eq[-1] == "graph":
         eq.remove("graph")
         break
 
@@ -13,18 +14,29 @@ screenSize = 500
 screen = pygame.display.set_mode((screenSize, screenSize))
 
 def yy(y):
-  return (-y + screenSize)
+    return -y + screenSize
 
-def px(x,y):
+def px(x, y):
     screen.set_at((int(x), int(y)), (255, 255, 255))
 
 def graph(i):
+    yp = 0
     for x in range(screenSize):
         y = eval(eq[i])
-        px(x, yy(y))
+        if (x == 0) or (int(yp) == int(y)):
+            px(x, yy(y))
+        else:
+            if int(yp) < int(y):
+                for w in range(int(yp + 1), int(y + 1)):
+                    px(x, yy(w))
+            elif int(yp) > int(y):
+                for w in range(int(y), int(yp)):
+                    px(x, yy(w))
+        yp = y
 
 while True:
     screen.fill((0, 0, 0))
     for i in range(len(eq)):
         graph(i)
     pygame.display.flip()
+
